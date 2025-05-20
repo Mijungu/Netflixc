5fc828939cd9da865a3208613f8a85585c9bfe41
Ishmael Antone Mijungu <antone@mac.home>
2025-05-19 22:15:19 -0400

    edit

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,17 +1,30 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
-
+  const [apiData,setApiData] = useState([])
   const cardsRef = useRef();
 
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTRjNThkOWFlNzEyZTBmMmNjMDU2OWUwMzY1MzhiNiIsIm5iZiI6MTc0NzM1NjU4MS4wMTgsInN1YiI6IjY4MjY4YmE1Yjc1NzM2N2I1MDJkMmE5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YYHLOYyibdajvK_L_0kI-XkIE6d3esbNL96vBD2RXYs'
+    }
+  };
+  
   const handleWheel = (event)=>{
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY; 
   }
   
   useEffect(()=>{
+
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
     cardsRef.current.addEventListener('wheel', handleWheel);
   },[])
 
@@ -19,10 +32,10 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index)=>{
+        {apiData.map((card, index)=>{
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
